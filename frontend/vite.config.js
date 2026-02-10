160fcadb77467d41c97766863cf5da36c2ffb18f
Kakha Kikalishvili <116180729+Reetryyy@users.noreply.github.com>
2026-02-10 14:33:53 +0400

    Add allowedHosts configuration to Vite server

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: process.env.VITE_ALLOWED_HOST
+      ? [process.env.VITE_ALLOWED_HOST]
+      : []
+  }
 })
