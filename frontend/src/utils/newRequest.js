1fee124c0f2715166c652fcc9db2f37800a88b81
Luka Tsiskarishvili <shashranluka@gmail.com>
2026-02-14 12:39:13 +0400

    fix: revert baseURL in newRequest to localhost for local development

--- frontend/src/utils/newRequest.js
+++ frontend/src/utils/newRequest.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const newRequest = axios.create({
   // baseURL: import.meta.env.VITE_API_URL || "https://dashni.dosh.ge/api/",
-  baseURL: "https://dashni.dosh.ge/api/",
+  baseURL: "http://localhost:8800/api/",
   withCredentials: true,
 });
 
