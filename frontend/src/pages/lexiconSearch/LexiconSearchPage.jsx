b360fc562fc0c2752909f89a40d958290deaa523
Luka Tsiskarishvili <shashranluka@gmail.com>
2026-06-15 22:26:05 +0400

    feat: add temporary message to LexiconSearchPage indicating unavailability

--- frontend/src/pages/lexiconSearch/LexiconSearchPage.jsx
+++ frontend/src/pages/lexiconSearch/LexiconSearchPage.jsx
@@ -4,7 +4,9 @@ import "./LexiconSearchPage.scss";
 function LexiconSearchPage() {
   return (
     <section className="lexicon-search-page">
+      <h1 style={{textAlign: "center", color: "#FF0000"}}>დროებით არ მუშაობს</h1>
       <LexiconSearch />
+      
     </section>
   );
 }
